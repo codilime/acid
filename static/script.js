1bfc2aa11c6d75671f0dc11d064a3092b5305164
Michal Milewicz <michal.milewicz.github@wp.pl>
2018-08-16 17:50:38 +0200

    seperate refresh to standalone function for using it inside block adding to each templeate needed refresh button

--- static/script.js
+++ static/script.js
@@ -1,11 +1,39 @@
 $(function () {
+
   $('[data-localtime="true"]').each(function () {
     let $element = $(this)
     let dateTime = moment.utc($element.text().trim())
     dateTime.local()
     $element.text(dateTime.format('YYYY-MM-DD HH:mm:ss'))
   })
+
   $('.no-collapsable').on('click', function (event) {
     event.stopImmediatePropagation()
   })
 })
+
+function refresh_me() {
+  var RefreshF = localStorage.getItem('refresh_f')
+  $('#auto-refresh-li').removeClass('d-none')
+  window.setInterval(refresh, 2000)
+  if (RefreshF == null) {
+    localStorage.setItem('refresh_f', 'is_off')
+  }
+  if (RefreshF === 'is_on' && !$('#auto-refresh').is(':checked')) {
+    $('#auto-refresh').click()
+  }
+  $('#auto-refresh').on('change', function (event) {
+    if (RefreshF === 'is_on') {
+      localStorage.setItem('refresh_f', 'is_off')
+    } else if (RefreshF === 'is_off') {
+      localStorage.setItem('refresh_f', 'is_on')
+    }
+  })
+
+  function refresh () {
+    if ($('#auto-refresh').is(':checked')) {
+      window.location.reload()
+    }
+  }
+
+}
