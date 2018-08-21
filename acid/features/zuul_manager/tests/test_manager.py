5a1862c291c81899cc94501a338e3cb315fe6db2
Kamil Mankowski <kamil.mankowski@codilime.com>
2018-08-21 11:45:04 +0200

    Move tests for zuul_manager

--- acid/features/zuul_manager/tests/test_manager.py
+++ acid/features/zuul_manager/tests/test_manager.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
-import unittest
-from unittest.mock import patch
-
 import pytest
 
 from ..manager import ZuulManager
 from ..exceptions import ZuulManagerConfig
-from .fixtures import path_to_test_file
 
 
 @pytest.mark.unit
-class TestZuulConnector(unittest.TestCase):
-    def test_raise_when_no_user_key_file(self):
-        with self.assertRaises(ZuulManagerConfig):
+class TestZuulConnector:
+    def test_raise_when_no_user_key_file(self, path_to_test_file):
+        with pytest.raises(ZuulManagerConfig):
             ZuulManager(host="host",
                         username="user",
                         user_key_file="there/is/no/file",
@@ -22,8 +18,8 @@ class TestZuulConnector(unittest.TestCase):
                         project="project",
                         policy="AutoAddPolicy")
 
-    def test_raise_when_no_host_keys_file(self):
-        with self.assertRaises(ZuulManagerConfig):
+    def test_raise_when_no_host_keys_file(self, path_to_test_file):
+        with pytest.raises(ZuulManagerConfig):
             ZuulManager(host="host",
                         username="user",
                         user_key_file=path_to_test_file("insecure_user_key"),
@@ -33,7 +29,7 @@ class TestZuulConnector(unittest.TestCase):
                         project="project",
                         policy="AutoAddPolicy")
 
-    def test_can_set_autoadd_policy(self):
+    def test_can_set_autoadd_policy(self, path_to_test_file):
         ZuulManager(host="host",
                     username="user",
                     user_key_file=path_to_test_file("insecure_user_key"),
@@ -43,7 +39,7 @@ class TestZuulConnector(unittest.TestCase):
                     project="project",
                     policy="AutoAddPolicy")
 
-    def test_can_set_reject_policy(self):
+    def test_can_set_reject_policy(self, path_to_test_file):
         ZuulManager(host="host",
                     username="user",
                     user_key_file=path_to_test_file("insecure_user_key"),
@@ -53,8 +49,8 @@ class TestZuulConnector(unittest.TestCase):
                     project="project",
                     policy="RejectPolicy")
 
-    def test_raise_when_try_to_set_not_exists_policy(self):
-        with self.assertRaises(ZuulManagerConfig):
+    def test_raise_when_try_to_set_not_exists_policy(self, path_to_test_file):
+        with pytest.raises(ZuulManagerConfig):
             ZuulManager(host="host",
                         username="user",
                         user_key_file=path_to_test_file("insecure_user_key"),
@@ -64,8 +60,8 @@ class TestZuulConnector(unittest.TestCase):
                         project="project",
                         policy="no-policy")
 
-    @patch.object(ZuulManager, '_run_command')
-    def test_enqueue_generate_correct_command(self, run_command):
+    def test_enqueue_generate_correct_command(self, path_to_test_file, mocker):
+        run_command = mocker.patch.object(ZuulManager, '_run_command')
         zuul = ZuulManager(host="host",
                            username="user",
                            user_key_file=path_to_test_file("insecure_user_key"),
@@ -81,8 +77,8 @@ class TestZuulConnector(unittest.TestCase):
             'periodic-nightly --project project --ref refs/head/master '
             '> /dev/null 2>&1 &')
 
-    @patch.object(ZuulManager, '_run_command')
-    def test_dequeue_generate_correct_command(self, run_command):
+    def test_dequeue_generate_correct_command(self, path_to_test_file, mocker):
+        run_command = mocker.patch.object(ZuulManager, '_run_command')
         zuul = ZuulManager(host="host",
                            username="user",
                            user_key_file=path_to_test_file("insecure_user_key"),
@@ -97,8 +93,9 @@ class TestZuulConnector(unittest.TestCase):
             'zuul dequeue --tenant tenant --pipeline periodic-nightly '
             '--project project --ref refs/head/master > /dev/null 2>&1 &')
 
-    @patch.object(ZuulManager, '_run_command')
-    def test_enqueue_correct_escape_insecure_args(self, run_command):
+    def test_enqueue_correct_escape_insecure_args(self, path_to_test_file,
+                                                  mocker):
+        run_command = mocker.patch.object(ZuulManager, '_run_command')
         zuul = ZuulManager(host="host",
                            username="user",
                            user_key_file=path_to_test_file("insecure_user_key"),
@@ -114,8 +111,9 @@ class TestZuulConnector(unittest.TestCase):
             '--trigger TRIGGER --pipeline \'periodic`who`\' --project PROJECT '
             '--ref \'refs/head/master???*\' > /dev/null 2>&1 &')
 
-    @patch.object(ZuulManager, '_run_command')
-    def test_dequeue_correct_escape_insecure_args(self, run_command):
+    def test_dequeue_correct_escape_insecure_args(self, path_to_test_file,
+                                                  mocker):
+        run_command = mocker.patch.object(ZuulManager, '_run_command')
         zuul = ZuulManager(host="host",
                            username="user",
                            user_key_file=path_to_test_file("insecure_user_key"),
