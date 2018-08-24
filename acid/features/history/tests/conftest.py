649fb606be8ab7da436310ab7280c0345857eb1b
Krzysztof Nagrodkiewicz <knagrodkiewicz@gmail.com>
2018-08-24 14:44:51 +0200

    Move test db to sqlite (#29)
    Use sqlite database to test models

--- acid/features/history/tests/conftest.py
+++ acid/features/history/tests/conftest.py
@@ -3,47 +3,57 @@ from datetime import datetime
 
 import pytest
 
-from pony.orm import db_session
+from pony.orm import commit, db_session
 
 from ..model import ZuulBuild, ZuulBuildSet
 
 
-@db_session
-@pytest.fixture
-def zuul_build_set(mocker):
-    mocker.patch('pony.orm.core.commit')
-    return ZuulBuildSet(zuul_ref='Zef2180cdc7ff440daefe48d85ed91b48',
-                        pipeline='periodic-nightly',
-                        project='acid-test-dev',
-                        change=None, patchset=None,
-                        ref='refs/heads/master',
-                        message='Build succeeded.',
-                        tenant='acid',
-                        result='SUCCESS',
-                        ref_url='http://acid.test/gitweb/',
-                        oldrev='', newrev='',
-                        builds=zuul_builds(mocker))
-
-
-@db_session
 @pytest.fixture
 def zuul_builds(mocker):
     mocker.patch('pony.orm.core.commit')
-    return [ZuulBuild(buildset_id=5010,
-                      uuid='86394a77c99f45aba0e299b660214a9c',
-                      job_name='acid-build-nightly',
-                      result='SUCCESS',
-                      start_time=datetime(2018, 2, 23, 22, 18, 15),
-                      end_time=datetime(2018, 2, 23, 23, 18, 46),
-                      voting=True,
-                      log_url='http://logs.acid.test/104/865548a7/',
-                      node_name='first_node'),
-            ZuulBuild(buildset_id=5010,
-                      uuid='86394a77c99f45aba0e299b660214a9c',
-                      job_name='acid-build-nightly',
-                      result='SUCCESS',
-                      start_time=datetime(2018, 2, 23, 22, 0, 0),
-                      end_time=datetime(2018, 2, 23, 23, 55, 0),
-                      voting=True,
-                      log_url='http://logs.acid.test/104/865548a7/',
-                      node_name='first_node')]
+    return [make_build(build_number=104, buildset_id=5010),
+            make_build(build_number=104, buildset_id=5010)]
+
+
+@pytest.fixture
+def make_buildset():
+    def _make_buildset(build_number, branch='master'):
+        buildset = ZuulBuildSet(zuul_ref='Zef2180cdc7ff440daefe48d85ed91b48',
+                                pipeline='periodic-nightly',
+                                project='acid-test-dev',
+                                change=None, patchset=None,
+                                ref=f'refs/heads/{branch}',
+                                message='Build succeeded.',
+                                tenant='acid',
+                                result='SUCCESS',
+                                ref_url='http://acid.test/gitweb/',
+                                oldrev='', newrev='')
+        commit()
+        make_build(build_number=build_number, buildset_id=buildset.id)
+        make_build(build_number=build_number, buildset_id=buildset.id)
+        return buildset
+
+    return _make_buildset
+
+
+@db_session
+def make_build(build_number, buildset_id):
+    return ZuulBuild(buildset_id=buildset_id,
+                     uuid='86394a77c99f45aba0e299b660214a9c',
+                     job_name='acid-build-nightly',
+                     result='SUCCESS',
+                     start_time=datetime(2018, 2, 23, 22, 0, 0),
+                     end_time=datetime(2018, 2, 23, 23, 55, 0),
+                     voting=True,
+                     log_url=f'http://logs.acid.test/{build_number}/865548a7/',
+                     node_name='first_node')
+
+
+@pytest.fixture
+def populate_database():
+    def _populate_database(count=10, branch='master', start_build_number=105):
+        return [make_buildset()(build_number=str(start_build_number + x),
+                                branch=branch)
+                for x in range(count)]
+
+    return _populate_database
--- acid/tests/__init__.py
+++ acid/tests/__init__.py
@@ -12,7 +12,20 @@ class TestWithAppContext:
         self._ctx.pop()
 
 
-class IntegrationTestCase(TestWithAppContext):
+class DatabaseTestCase:
+    def setup_method(self, method):
+        try:
+            db.db.bind(provider='sqlite', filename=':memory:')
+            db.db.generate_mapping(create_tables=True)
+        except TypeError:
+            pass
+
+    def teardown_method(self, method):
+        db.db.drop_all_tables(with_all_data=True)
+        db.db.create_tables()
+
+
+class IntegrationTestCase(TestWithAppContext, DatabaseTestCase):
     def setup_method(self, method):
         super().setup_method(method)
         db.connect()
