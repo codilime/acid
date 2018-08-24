e1b36ba8bea4e3d96ab06e31751a0dda005a9704
MichalMilewicz <42034041+MichalMilewicz@users.noreply.github.com>
2018-08-24 12:06:38 +0200

    Auto refresh button (#18)
    Add auto refresh button to pages

--- static/script.js
+++ static/script.js
@@ -5,7 +5,33 @@ $(function () {
     dateTime.local()
     $element.text(dateTime.format('YYYY-MM-DD HH:mm:ss'))
   })
+
   $('.no-collapsable').on('click', function (event) {
     event.stopImmediatePropagation()
   })
 })
+
+function enableAutoRefresh () { // eslint-disable-line no-unused-vars
+  let refreshFlag = sessionStorage.getItem('refreshTag')
+  $('#auto-refresh-li').removeClass('d-none')
+  window.setInterval(refresh, 15000)
+  if (refreshFlag == null) {
+    sessionStorage.setItem('refreshTag', 'is_off')
+  }
+  if (refreshFlag === 'is_on' && !$('#auto-refresh').is(':checked')) {
+    $('#auto-refresh').click()
+  }
+  $('#auto-refresh').on('change', function (event) {
+    if (sessionStorage.getItem('refreshTag') === 'is_on') {
+      sessionStorage.setItem('refreshTag', 'is_off')
+    } else {
+      sessionStorage.setItem('refreshTag', 'is_on')
+    }
+  })
+
+  function refresh () {
+    if ($('#auto-refresh').is(':checked')) {
+      window.location.reload()
+    }
+  }
+}
