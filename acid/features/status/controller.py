e0049b42a1d5a10e32bc2d858e5f84679d2b7777
dariusz.markiewicz <dariusz.markiewicz@codilime.com>
2018-09-20 15:15:00 +0200

    Fix review

--- acid/features/status/controller.py
+++ acid/features/status/controller.py
@@ -10,11 +10,12 @@ status = Blueprint('status', __name__, template_folder='../../templates')
 @status.route('/status')
 @status.route('/status/<string:pipename>')
 def show_status(pipename=None):
-    zuul_url = current_app.config["zuul"]["zuul_server"]
-    zuul_endpoint = current_app.config['zuul']['status_endpoint']
+    config = current_app.config
+    zuul_url = config["zuul"]["url"]
+    zuul_endpoint = config['zuul']['status_endpoint']
 
     pipename = (pipename if pipename is not None else
-                current_app.config['default']['pipename'])
+                config['default']['pipename'])
     url = service.status_endpoint(zuul_url, zuul_endpoint)
     resource = service.fetch_json_data(endpoint=url)
     queues = service.make_queues(resource['pipelines'], pipename, zuul_url)
@@ -27,11 +28,12 @@ def show_status(pipename=None):
 
 @status.route('/')
 def show_dashboard():
-    zuul_url = current_app.config["zuul"]["zuul_server"]
-    zuul_endpoint = current_app.config['zuul']['status_endpoint']
+    config = current_app.config
+    zuul_url = config["zuul"]["url"]
+    zuul_endpoint = config['zuul']['status_endpoint']
 
     url = service.status_endpoint(zuul_url, zuul_endpoint)
     resource = service.fetch_json_data(endpoint=url)
     pipeline_stats = service.pipelines_stats(
-        resource['pipelines'], current_app.config['zuul']['pipelines'])
+        resource['pipelines'], config['zuul']['pipelines'])
     return render_template('dashboard.html', pipeline_stats=pipeline_stats)
